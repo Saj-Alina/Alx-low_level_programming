93c50b251d632e70c12b8d5b0a47e47b4c233a7f
Saj-Alina <alinasalihu@gmail.com>
2022-12-09 05:29:01 -0800

    C language

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *   *
  *    *Description: We have been tasked with using printf
  *     *Return: Shouldn't return anything (0)
- *      */
+       */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
